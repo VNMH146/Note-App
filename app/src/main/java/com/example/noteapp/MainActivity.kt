8eef47ada17b65e0a5ca624238a0e6d4b7dc920c
Huy <vohuy060214@gmail.com>
2024-12-06 17:55:28 +0700

    update task dialog

--- app/src/main/java/com/example/noteapp/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.noteapp
 
+import android.app.Dialog
 import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
+import android.view.LayoutInflater
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,38 +16,43 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.noteapp.databinding.ActivityMainBinding
 import com.example.noteapp.ui.theme.NoteAppTheme
+import com.example.noteapp.utils.setupDialog
 
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            NoteAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+class MainActivity : AppCompatActivity() {
+
+    private val mainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val addTaskDialog : Dialog by lazy {
+        Dialog(this).apply {
+            setupDialog(R.layout.add_task_dialog)
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NoteAppTheme {
-        Greeting("Android")
+    private val updateTaskDialog : Dialog by lazy {
+        Dialog(this).apply {
+            setupDialog(R.layout.update_task_dialog)
+        }
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(mainBinding.root)
+
+
+        val addCloseImg = addTaskDialog.findViewById<ImageView>(R.id.closeImg)
+        val updateCloseImg = updateTaskDialog.findViewById<ImageView>(R.id.closeImg)
+
+        addCloseImg.setOnClickListener{ addTaskDialog.dismiss() }
+        updateCloseImg.setOnClickListener{ updateTaskDialog.dismiss() }
+
+        mainBinding.addTaskFABtn.setOnClickListener{
+            addTaskDialog.show()
+        }
+    }
+
 }
